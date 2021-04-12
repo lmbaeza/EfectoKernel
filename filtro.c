d2d3b2c7b5b25dfe75ff5640b9c4bb07e797236e
sorkierkegaard <lmbaeza@unal.edu.co>
2021-04-12 15:55:39 -0500

    Filtro para Quitar ruido fué agregado

--- filtro.c
+++ filtro.c
@@ -8,6 +8,23 @@ char* IMAGEN_SALIDA;
 int ARG;
 int NUM_HILOS;
 
+
+int cmp(const void *_a, const void *_b) {
+    float *a, *b;
+    a = (float*) _a;
+    b = (float*) _b;
+    return (*a - *b);
+}
+
+#define eps  0.000000001
+
+int compare(const void *_a, const void *_b) {
+    float *a, *b;
+    a = (float*) _a;
+    b = (float*) _b;
+    return ( (((*a) + eps) < (*b))? -1 :( (((*b) + eps) < (*a) )? 1 : 0) );
+}
+
 int main(int argc, char *argv[]) {
 
     if(argc != 5) {
@@ -26,22 +43,38 @@ int main(int argc, char *argv[]) {
     sod_img imgIn = sod_img_load_from_file(IMAGEN_ENTRADA, SOD_IMG_COLOR);
 
     if (imgIn.data == 0) {
-		// Validar que la imagen exista
-		printf("No pudo cargar la imagen %s\n", IMAGEN_ENTRADA);
-		return 0;
-	}
+        // Validar que la imagen exista
+        printf("No pudo cargar la imagen %s\n", IMAGEN_ENTRADA);
+        return 0;
+    }
 
     // Modificar la imagen
-    for(int i = 0; i  < 100; ++i) {
-        for(int j = 0; j < 100; ++j) {
-            sod_img_set_pixel(imgIn, i, i+j, 0, 0.3434);
+    const int TAM_COLORES = 9;
+    float colores[9];
+
+    for(int i = 1; i+1 < imgIn.w; ++i) {
+        for(int j = 1; j+1 < imgIn.h; ++j) {
+            // float sod_img_get_pixel(sod_img input, int x, int y, int c);
+            colores[0] = sod_img_get_pixel(imgIn, i-1, j-1, 0);
+            colores[1] = sod_img_get_pixel(imgIn, i-1, j, 0);
+            colores[2] = sod_img_get_pixel(imgIn, i-1, j + 1, 0);
+            colores[3] = sod_img_get_pixel(imgIn, i, j-1, 0);
+            colores[4] = sod_img_get_pixel(imgIn, i, j, 0);
+            colores[5] = sod_img_get_pixel(imgIn, i, j + 1, 0);
+            colores[6] = sod_img_get_pixel(imgIn, i + 1, j-1, 0);
+            colores[7] = sod_img_get_pixel(imgIn, i + 1, j, 0);
+            colores[8] = sod_img_get_pixel(imgIn, i + 1, j + 1, 0);
+
+            qsort(colores, TAM_COLORES, sizeof(float), &compare);
+            int idx = 4;
+            sod_img_set_pixel(imgIn, i, j, 0,colores[idx]);
         }
     }
 
-	sod_img_save_as_png(imgIn, IMAGEN_SALIDA);
+    sod_img_save_as_png(imgIn, IMAGEN_SALIDA);
 
     // Liberar la Memoria
-	sod_free_image(imgIn);
+    sod_free_image(imgIn);
 
     return 0;
 }
